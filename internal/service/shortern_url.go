b493e37156df02dd91997f11b5f2ab6406eecd1f
dungi3 <tranquocdung1998.i3@gmail.com>
2026-07-12 23:47:55 +0700

    remove error

--- internal/service/shortern_url.go
+++ internal/service/shortern_url.go
@@ -13,7 +13,6 @@ import (
 
 type ShorternUrl interface {
 	ShortenUrlShortenUrl(ctx context.Context, url string, exp int64) (string, error)
-	GetLinkFromCode(ctx context.Context, code string) (string, error)
 }
 
 type shorternUrl struct {
@@ -48,17 +47,4 @@ func (s *shorternUrl) ShortenUrlShortenUrl(ctx context.Context, url string, exp 
 	}
 
 	return randomCode, nil
-}
-
-var ErrCodeDoesntExist = errors.New("code does not exist")
-
-// GetLinkFromCode return the original from shorten code
-func (s *shorternUrl) GetLinkFromCode(ctx context.Context, code string) (string, error) {
-	link, err := s.repository.GetURL(ctx, code)
-	if errors.Is(err, redis.Nil) {
-		// try - catch none exit code exception
-		return "", ErrCodeDoesntExist
-	}
-
-	return link, err
 }
