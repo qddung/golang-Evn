0e954ce37ddb41485d7813194ce54efb462626a3
dungi3 <tranquocdung1998.i3@gmail.com>
2026-08-17 22:36:18 +0700

    intergrate test login

--- internal/api/api.go
+++ internal/api/api.go
@@ -119,6 +119,7 @@ func (e *engine) initRoutes(cfg *config.Config) {
 		v1Routes.POST("/links/shorten", allHandlers.shorten.ShortenUrl)
 		v1Routes.GET("/links/redirect/:code", allHandlers.shorten.Redirect)
 		v1Routes.POST("/users/register", allHandlers.user.Register)
+		v1Routes.Use(jwtMiddeleware.JwtAuth()) // middelware
 
 		v1Routes.Use(jwtMiddeleware.JwtAuth()) // middelware
 		v1Routes.GET("/self/info", allHandlers.user.GetUserInfo)
