af1cf33c605bd261c30f3be3385af376f8bbc2a0
dungi3 <tranquocdung1998.i3@gmail.com>
2026-09-15 20:00:29 +0700

    remove duplicate key

--- internal/models/entity/bookmark.go
+++ internal/models/entity/bookmark.go
@@ -10,8 +10,8 @@ type Bookmark struct {
 	base.Base
 	Code        string `json:"code" gorm:"type:text;"`
 	Description string `json:"description" gorm:"type:text"`
-	Url         string `json:"url" gorm:"not null;type:text;index:idx_id,unique"`
-	UserId      string `json:"user_id" gorm:"not null;type:uuid;index:idx_id,unique"`
+	Url         string `json:"url" gorm:"not null;type:text"`
+	UserId      string `json:"user_id" gorm:"not null;type:uuid"`
 	CodeInt     int    `json:"-" gorm:"not null;autoincrement;index:idx_code,unique"`
 	// Navigation Attribute
 	User User `gorm:foreignKey:UserId`
