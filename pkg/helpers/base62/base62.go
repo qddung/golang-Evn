448f3d45f2e75aff7fa0ca2d39e2f57891ffac04
dungi3 <tranquocdung1998.i3@gmail.com>
2026-09-19 00:21:41 +0700

    move base62 library

--- pkg/helpers/base62/base62.go
+++ pkg/helpers/base62/base62.go
@@ -1,7 +1,8 @@
 package base62_helper
 
 import (
-	"github.com/ivanrad/base62"
+	base62_lib "github.com/homework/lab/pkg/lib/base62"
+	_ "github.com/ivanrad/base62"
 )
 
 //go:generate mockery --name=Base62Helper --filename=base62.go --outpkg=mocks
@@ -10,13 +11,14 @@ type Base62Helper interface {
 }
 
 type base62Helper struct {
+	base62Encoding base62_lib.Base62Encoding
 }
 
 func (b *base62Helper) Encode(data string) string {
-	res := base62.EncodeToString([]byte(data))
+	res := b.base62Encoding.EncodeToString([]byte(data))
 	return res
 }
 
-func New() Base62Helper {
-	return &base62Helper{}
+func New(base62Encoding base62_lib.Base62Encoding) Base62Helper {
+	return &base62Helper{base62Encoding}
 }
